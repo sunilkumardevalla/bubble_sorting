9f2086192d54a0dd349d27b9e93c6592519974d2
DsunilK <sunilkumardevalla@gmail.com>
2021-01-15 15:03:32 -0500

    add sorting for num

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,14 @@
 def bubble_sort(array)
-  do 
+  array.length.times do
+    array.each_with_index do |value, index|
+      next unless array[index + 1]
+      next if value < array[index + 1]
+      array[index], array[index + 1] = array[index + 1], array[index]
+    end
   end
-  
+  array
+end
+
+array = [3, 1, 5, 9, 4, 7, 1, 2, 5, 9, 8]
+
+puts bubble_sort(array)
