00bf96aa859eed24d55fd70914b9508e410c1075
DsunilK <sunilkumardevalla@gmail.com>
2021-01-15 20:40:06 -0500

    Add end result

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,3 +12,21 @@ end
 array = [3, 1, 5, 9, 4, 7, 1, 2, 5, 9, 8]
 
 puts bubble_sort(array)
+
+def bubble_sort_by(array)
+  puts array if array.length <= 1
+  n = array.length - 1
+  swapped = true
+  while swapped
+    swapped = false
+    n.times do |i|
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+  end
+  array
+end
+
+puts bubble_sort_by(%w[Sunil Sunny hey hi hello]) {| left, right | left.length - right.length}
